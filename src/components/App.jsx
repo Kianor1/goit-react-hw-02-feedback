ed07ff3bcc124b48cfa32d3046b639927c33601c
B.O. Vdovych <123995120+Kianor1@users.noreply.github.com>
2024-02-15 16:15:00 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import
+
 export const App = () => {
   return (
     <div
@@ -7,10 +11,8 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
-    >
-      React homework template
-    </div>
+    ></div>
   );
 };
