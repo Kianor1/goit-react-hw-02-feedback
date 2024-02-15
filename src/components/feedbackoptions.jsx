21dd5300a316d1016b6622f71e7bb6f2c5e3b881
B.O. Vdovych <123995120+Kianor1@users.noreply.github.com>
2024-02-16 00:52:30 +0200

    fix & build

--- src/components/feedbackoptions.jsx
+++ src/components/feedbackoptions.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import s from "./feedback.module.css"
 import { nanoid } from 'nanoid';
 
 export default function FeedbackOptions({ options, onLeaveFeedback }) {
   return (
-    <div>
+    <div className={s.buttons}>
       {options.map(option => (
         <button key={nanoid()} onClick={() => onLeaveFeedback(option)}>
           {option}
